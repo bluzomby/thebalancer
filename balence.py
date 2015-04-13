395cb55812d351148201829a30364bef4a1d161e
bluzomby <jakemassoth@me.com>
2015-04-13 13:30:30 +0300

    Set up more window-related things

--- balence.py
+++ balence.py
@@ -6,3 +6,7 @@ import pygame
 from pygame.locals import *
 pygame.init()
 screen = pygame.display.set_mode((1200, 800)) # set the screen to be a 480p windowed screen
+# set up some time stuff 
+clock = pygame.time.Clock()
+FRAMES_PER_SECOND = 60 # pcmr
+deltat = clock.tick(FRAMES_PER_SECOND) 
