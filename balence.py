6a1d0f9767ac9f84cb8aa92fee33ee4bec9708fa
bluzomby <jakemassoth@me.com>
2015-04-14 15:56:44 +0300

    made a loop so the program doesn't close immediately
    Also added a background color and an image for a text box

--- balence.py
+++ balence.py
@@ -4,9 +4,24 @@ import os
 import sys
 import pygame
 from pygame.locals import *
+# General window intializing stuff
 pygame.init()
-screen = pygame.display.set_mode((1200, 800)) # set the screen to be a 480p windowed screen
+# set the background color
+background_color = (255,255,255)
+screen = pygame.display.set_mode((1280, 720)) # set the screen to be a 480p windowed screen
+pygame.display.set_caption('The Balancer')
+screen.fill(background_color)
 # set up some time stuff 
 clock = pygame.time.Clock()
 FRAMES_PER_SECOND = 60 # pcmr
-deltat = clock.tick(FRAMES_PER_SECOND) 
+deltat = clock.tick(FRAMES_PER_SECOND)
+# load images 
+text = pygame.image.load('textbox.bmp')
+screen.blit(text, (50, 100))
+pygame.display.flip()
+# make sure the window doesn't close immediately by creating an infine loop
+running = True
+while running:
+	for event in pygame.event.get(): # make it so the window closes if the "X" is pressed
+		if event.type == pygame.QUIT:
+			running = False
