7f306bf079d9169368f039b62ade68c0a731962a
bluzomby <jakemassoth@me.com>
2015-04-19 10:09:20 +0300

    Cleaned up some code

--- balence.py
+++ balence.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
-import math
-import os
-import sys
-import pygame
+import math, os, sys, pygame
 from pygame.locals import *
 # General window intializing stuff
 pygame.init()
 # set the background color
-background_color = (255,255,255)
-screen = pygame.display.set_mode((1280, 720)) # set the screen to be a 480p windowed screen
-pygame.display.set_caption('The Balancer')
+background_color = (255,255,255) # white
+screen = pygame.display.set_mode((1280, 720)) # set the screen to be a 720p windowed screen
+pygame.display.set_caption('The Balancer') 
 screen.fill(background_color)
 # set up some time stuff 
 clock = pygame.time.Clock()
 FRAMES_PER_SECOND = 60 # pcmr
 deltat = clock.tick(FRAMES_PER_SECOND)
-# load images 
-text = pygame.image.load('textbox.bmp')
-screen.blit(text, (50, 100))
+# load images here
 pygame.display.flip()
 # make sure the window doesn't close immediately by creating an infine loop
 running = True
